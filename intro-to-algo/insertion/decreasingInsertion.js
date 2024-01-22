eb9c597d789e10f69edc26ab716300516db55b45
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2024-01-21 21:56:01 -0500

    Intro to Algo

---
+++ intro-to-algo/insertion/decreasingInsertion.js
@@ -0,0 +1,15 @@
+function decInsertion(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let key = arr[i];
+    let j = i - 1;
+    while (j >= 0 && key > arr[j]) {
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+    arr[j + 1] = key;
+  }
+  return arr;
+}
+
+const arr = [3, 1, 2, 15, 20];
+console.log(decInsertion(arr));
---
+++ intro-to-algo/insertion/insertion.js
@@ -0,0 +1,20 @@
+// Insertion Sort
+
+const insertionSort = (arr) => {
+  for (let i = 1; i < arr.length; i++) {
+    let key = arr[i];
+    let j = i - 1;
+
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+    arr[j + 1] = key;
+  }
+  return arr;
+};
+
+const arr = [3, 1, 2, 20, 15];
+console.log(insertionSort(arr));
+
+// Big - O : n^2
