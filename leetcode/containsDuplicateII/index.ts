deafc8fb8baea364854c204c809e515928494363
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2024-02-17 19:01:38 -0500

    ContainsDuplicateII - Optimal Solution

--- leetcode/containsDuplicateII/index.ts
+++ leetcode/containsDuplicateII/index.ts
@@ -1,11 +1,35 @@
+//Approach good for smaller-medium arrays
+//Complexity - O(n^2)
+
+// function containsNearbyDuplicate(nums: number[], k: number): boolean {
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] === nums[j] && Math.abs(i - j) <= k) {
+//         return true;
+//       }
+//     }
+//   }
+//   return false;
+// }
+
+// const number_array: number[] = [1, 2, 3, 1, 2, 3];
+// const k: number = 2;
+
+// console.log(containsNearbyDuplicate(number_array, k));
+
+//Optimal approach
+//Complexity - O(n)
 function containsNearbyDuplicate(nums: number[], k: number): boolean {
+  const numMap = new Map();
+
   for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] === nums[j] && Math.abs(i - j) <= k) {
-        return true;
-      }
+    if (numMap.has(nums[i]) && Math.abs(i - numMap.get(nums[i])) <= k) {
+      return true;
+    } else {
+      numMap.set(nums[i], i);
     }
   }
+
   return false;
 }
 
