3144ccdab1e3c7aef87778f1c450ea69ec72a205
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2024-03-08 22:34:14 -0500

    Min Common Val

---
+++ leetcode/minCommonVal/index.ts
@@ -0,0 +1,39 @@
+// Approach 1
+
+// function getCommon(nums1: number[], nums2: number[]): number {
+//   let commonElements: number[] = [];
+//   for (let i = 0; i < nums1.length; i++) {
+//     for (let j = 0; j < nums2.length; j++) {
+//       if (nums1[i] === nums2[j]) {
+//         commonElements.push(nums1[i]);
+//       }
+//     }
+//   }
+
+//   if (commonElements.length) {
+//     return commonElements.sort((a, b) => a - b)[0];
+//   }
+
+//   return -1;
+// }
+
+// Approach 2
+
+function getCommon(nums1: number[], nums2: number[]): number {
+  let i = 0,
+    j = 0;
+  while (i < nums1.length && j < nums2.length) {
+    if (nums1[i] === nums2[j]) {
+      return nums1[i];
+    } else if (nums1[i] < nums2[j]) {
+      i++;
+    } else {
+      j++;
+    }
+  }
+  return -1;
+}
+
+const nums1: number[] = [1, 2, 3];
+const nums2: number[] = [2, 3, 4];
+console.log(getCommon(nums1, nums2));
