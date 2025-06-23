f2a5ec009f8b48a16215fde4510d622ff1e78fca
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2025-06-23 19:05:18 -0400

    Implement candy function to distribute candies based on ratings

---
+++ leetcode_py/13.candies.py
@@ -0,0 +1,14 @@
+def candy(ratings):
+    candies = [1] * len(ratings)
+
+    for i in range(1, len(ratings)):
+        if ratings[i] > ratings[i-1]:
+            candies[i] = candies[i-1] + 1
+
+    for i in range(len(ratings)-2, -1, -1):
+        if ratings[i] > ratings[i+1]:
+            candies[i] = max(candies[i], candies[i+1] +1)
+
+    return sum(candies)
+
+print(candy([1, 0, 2]))  # Output: 5
